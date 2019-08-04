1cc26d546ecb4526762aa5b93a4e9d3f26f80242
Dean_F <stone.feng1990@gmail.com>
2019-08-04 11:56:06 +0800

    dateComponent

--- DRCategories/Classes/Foundation/NSDateComponents+DRExtension.h
+++ DRCategories/Classes/Foundation/NSDateComponents+DRExtension.h
@@ -13,6 +13,23 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)componentsFromTimeInterval:(int64_t)timeInterval;
 
++ (instancetype)componentsWithYear:(NSInteger)year
+                             month:(NSInteger)month
+                               day:(NSInteger)day;
+
++ (instancetype)componentsWithYear:(NSInteger)year
+                             month:(NSInteger)month
+                               day:(NSInteger)day
+                              hour:(NSInteger)hour
+                            minute:(NSInteger)minte
+                            second:(NSInteger)second;
+
++ (instancetype)lunarComponentsWithEra:(NSInteger)era
+                                  year:(NSInteger)year
+                                 month:(NSInteger)month
+                                   day:(NSInteger)day
+                             leapMonth:(BOOL)leapMonth;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- DRCategories/Classes/Foundation/NSDateComponents+DRExtension.m
+++ DRCategories/Classes/Foundation/NSDateComponents+DRExtension.m
@@ -22,4 +22,44 @@
     return components;
 }
 
++ (instancetype)componentsWithYear:(NSInteger)year
+                             month:(NSInteger)month
+                               day:(NSInteger)day {
+    NSDateComponents *cmp = [NSDateComponents new];
+    cmp.year = year;
+    cmp.month = month;
+    cmp.day = day;
+    return cmp;
+}
+
++ (instancetype)componentsWithYear:(NSInteger)year
+                             month:(NSInteger)month
+                               day:(NSInteger)day
+                              hour:(NSInteger)hour
+                            minute:(NSInteger)minte
+                            second:(NSInteger)second {
+    NSDateComponents *cmp = [NSDateComponents new];
+    cmp.year = year;
+    cmp.month = month;
+    cmp.day = day;
+    cmp.hour = hour;
+    cmp.minute = minte;
+    cmp.second = second;
+    return cmp;
+}
+
++ (instancetype)lunarComponentsWithEra:(NSInteger)era
+                                  year:(NSInteger)year
+                                 month:(NSInteger)month
+                                   day:(NSInteger)day
+                             leapMonth:(BOOL)leapMonth {
+    NSDateComponents *cmp = [NSDateComponents new];
+    cmp.era = era;
+    cmp.year = year;
+    cmp.month = month;
+    cmp.day = day;
+    cmp.leapMonth = leapMonth;
+    return cmp;
+}
+
 @end
