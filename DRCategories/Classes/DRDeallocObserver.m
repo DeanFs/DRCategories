461df0e37c98b1eec98fb1e6cb3860839711158d
Dean_F <247159603@qq.com>
2020-03-25 20:10:51 +0800

    fix

--- DRCategories/Classes/DRDeallocObserver.m
+++ DRCategories/Classes/DRDeallocObserver.m
@@ -29,6 +29,24 @@
     return obs;
 }
 
+/// 绑定宿主，监听宿主销毁
+/// @param parasitifer 宿主
+/// @param target 需要跟随宿主销毁而做一些操作的对象，可为nil
+/// @param block 宿主销毁的回调
++ (instancetype)associateParasitifer:(id)parasitifer
+                              target:(id)target
+                        deallocBlock:(void(^)(id target, id parasitifer))block {
+    if (parasitifer == nil) {
+        return nil;
+    }
+    DRDeallocObserver *obs = [DRDeallocObserver new];
+    obs.target = target;
+    obs.parasitifer = parasitifer;
+    obs.deallocBlock2 = block;
+    objc_setAssociatedObject(parasitifer, _cmd, obs, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    return obs;
+}
+
 - (void)setTarget:(id)target {
     m_target = target;
 }
@@ -49,21 +67,6 @@
     kDR_SAFE_BLOCK(self.deallocBlock2, m_target, m_parasitifer);
     m_target = nil;
     m_parasitifer = nil;
-}
-
-/// 绑定宿主，监听宿主销毁
-/// @param parasitifer 宿主
-/// @param target 需要跟随宿主销毁而做一些操作的对象，可为nil
-/// @param block 宿主销毁的回调
-+ (instancetype)associateParasitifer:(id)parasitifer
-                              target:(id)target
-                        deallocBlock:(void(^)(id target, id parasitifer))block {
-    DRDeallocObserver *obs = [DRDeallocObserver new];
-    obs.target = target;
-    obs.parasitifer = parasitifer;
-    obs.deallocBlock2 = block;
-    objc_setAssociatedObject(parasitifer, _cmd, obs, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    return obs;
 }
 
 @end
